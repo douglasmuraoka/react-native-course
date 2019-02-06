2b9f5a31f6f2745f9c2669f2b26532ca9c72d0d7
Douglas Muraoka <douglas.muraoka@gmail.com>
2019-02-06 21:16:01 -0200

    feat: Add Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import PropTypes from 'prop-types';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { thumbnailContainerStyle, thumbnailStyle, headerContentStyle, headerTextStyle, imageStyle } = styles;
   return (
     <Card>
@@ -20,6 +21,11 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
